9d241ad947b260bf8341b31eb9ba6ecca168d74e
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-01-12 15:16:44 +0800

    updating associationsemployee with hierarchy

--- app/models/hierarchy.rb
+++ app/models/hierarchy.rb
@@ -1,2 +1,3 @@
 class Hierarchy < ApplicationRecord
+   has_many :employees
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :hierarchies
   resources :employees
+  resources :hierarchies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
