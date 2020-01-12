e1115280418963d47c4067c4827181f9f85a062c
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-01-12 15:21:03 +0800

    migration to add ancestry field to hierarchy, add has_ancestry to hierarchy model

--- app/models/hierarchy.rb
+++ app/models/hierarchy.rb
@@ -1,3 +1,4 @@
 class Hierarchy < ApplicationRecord
+   has_ancestry
    has_many :employees
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_12_071204) do
+ActiveRecord::Schema.define(version: 2020_01_12_071813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
